dd4607832e745bf5eae899c9cf0d07b3945ca26a
azizikri <azizikri@gmail.com>
2023-05-19 10:43:42 +0700

    feat: alert component

---
+++ resources/js/Components/Alert.vue
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+<template>
+    <div v-if="$page.props.flash.error" class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+        <span class="font-medium">Error!</span> {{ $page.props.flash.error }}
+    </div>
+    <div v-if="$page.props.flash.success" class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
+        <span class="font-medium">Success!</span> {{ $page.props.flash.success }}
+    </div>
+</template>
+
+
